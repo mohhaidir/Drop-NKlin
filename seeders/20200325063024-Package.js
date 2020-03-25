f4bbde7162bc62e7c6f77ba3b67b6766e8fbdd7c
mohhaidir <khoiruljmbr@gmail.com>
2020-03-25 13:57:26 +0700

    commit master refix

--- seeders/20200325063024-Package.js
+++ seeders/20200325063024-Package.js
@@ -8,10 +8,30 @@ module.exports = {
 
       Example:
       */
-    return queryInterface.bulkInsert('Package', [
+    return queryInterface.bulkInsert('Packages', [
       {
-        name: 'John Doe',
-        price: 1000
+        name: 'Express',
+        price: 12000,
+        duration: 2,
+        urlIMG: null,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Regular',
+        price: 6000,
+        duration: 5,
+        urlIMG: null,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Santuy',
+        price: 10000,
+        duration: 7,
+        urlIMG: null,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], {});
   },
@@ -23,6 +43,6 @@ module.exports = {
 
       Example:
       */
-    return queryInterface.bulkDelete('Package', null, {});
+    return queryInterface.bulkDelete('Packages', null, {});
   }
 };
